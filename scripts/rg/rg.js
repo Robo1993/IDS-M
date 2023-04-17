cb99af3bbec3850243369da62bf9f4d8948da9b4
adgohar <114059470+adgohar@users.noreply.github.com>
2023-04-17 18:30:26 +0200

    Update Reaktionsgeschwindigkeit
    Changed test to have the sun appear at multiple positions

--- scripts/rg/rg.js
+++ scripts/rg/rg.js
@@ -2,6 +2,8 @@
 const standard_delay = [2422, 893, 4970, 3281, 547, 4340, 1436, 2182, 1268, 1318, 378, 1710, 383, 4698, 2338, 3178, 253, 700, 2007, 2004];
 const demo_delay = [1268, 547, 2422, 3281, 4970, 1436, 2338, 700, 2004, 2751];
 const pseudo_delay = [0];
+const x_values = [669, 387, 866, 267, 834, 747, 582, 666, 806, 811, 92, 847, 484, 380, 457, 228, 682, 871, 294];
+const y_values = [295, 1138, 616, 1033, 215, 981, 1119, 781, 685, 1009, 1147, 282, 838, 1042, 583, 183, 576, 420, 571];
 let delay;
 let item = 0;
 let counter;
@@ -12,6 +14,7 @@ let reaction_times = new Array();
 let sun_start;
 
 function initRG() {
+	$("#crossair").fadeOut(0);
 	if(questionCode.indexOf("H") != -1) {
 		$("#item-area").css("transform", "rotate(180deg)");
 		unLock();
@@ -44,13 +47,15 @@ function initRG() {
 				$("#crossair").fadeOut(400);
 				evaluateRG();
 			}else {
+				$("#sun-img").css("left", x_values[item]);
+				$("#sun-img").css("top", y_values[item]);
 				nextLoad();
 			}
 		});
 
 		$("#crossair").on("click", function() {
 			tap_counter++;
-			if(tap_counter >= 5) {
+			/*if(tap_counter >= 5) {
 				restart = true;
 				item = 0;
 				reaction_times = [];
@@ -62,9 +67,9 @@ function initRG() {
 				$("#play-button").click();
 				$("#center-area").css("display", "block");
 				$("#warning-restart").css("display", "block");
-			}else {
+			}else {*/
 				false_positives++;
-			}
+			//}
 		});
 	}
 }
